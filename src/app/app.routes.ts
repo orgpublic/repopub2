401a134ed3ca700041b1d156a21dcc80963aca02
Pankaj Gupta <mail2panki@gmail.com>
2026-06-20 21:35:20 +0530

    data binding initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { User } from './components/user/user';
+import { Admin } from './components/admin/admin';
+import { DataBinding } from './components/data-binding/data-binding';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', component: Admin },
+  { path: 'user', component: User },
+  { path: 'data-binding', component : DataBinding }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { User } from './components/user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,User,Admin],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
