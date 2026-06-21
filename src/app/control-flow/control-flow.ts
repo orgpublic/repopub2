486e97375e776762cde2a512c767a99f87185074
Pankaj Gupta <mail2panki@gmail.com>
2026-06-21 12:40:59 +0530

    linked signal changes

--- src/app/control-flow/control-flow.ts
+++ src/app/control-flow/control-flow.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, linkedSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
   imports: [CommonModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ControlFlow {
   isTrue: boolean = true;
   color: string = 'red';
   btnClass: string = 'btn btn-primary';
   style = { 'background-color': 'yellow' };
-  valueFromSignal = signal("This is from signal");
-  value:string = "This is from variable";
+  valueFromSignal = signal('This is from signal');
+  value: string = 'This is from variable';
+  linkedSignalvariable = linkedSignal({
+    source: this.valueFromSignal,
+    computation: (value) => "linked signal value is :" + this.valueFromSignal(),
+    // equal: (a, b) => a === b
+  });
 }
